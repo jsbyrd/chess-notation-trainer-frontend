b4ec0e635c2c0b028d2afcc33b089fd8baa0e287
Joshua Byrd <jsbyrd02@gmail.com>
2024-09-03 20:45:19 -0400

    Configure TailwindCSS + shadcn-ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
@@ -7,6 +8,11 @@ export default defineConfig({
   server: {
     watch: {
       usePolling: true,
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
