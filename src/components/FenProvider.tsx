3fbcc8d34893f4f34dad57c5ed9cb0411b4b2233
Joshua Byrd <jsbyrd02@gmail.com>
2024-09-18 22:25:43 -0400

    Implement color choice (i.e. MakeThatMove now only chooses white, black, or random based on user choice) and add arrows after every move indicating the correct choice

--- src/components/FenProvider.tsx
+++ src/components/FenProvider.tsx
@@ -1,19 +1,38 @@
-import { createContext, ReactNode, useContext } from "react";
+import { createContext, ReactNode, useContext, useMemo } from "react";
 import { fenStrings } from "@/utils/fenStrings";
+import { ChessColor } from "./GameOptionProvider";
 
 interface Fen {
-  getRandomPosition: () => string;
+  getRandomPosition: (color: ChessColor) => string;
 }
 
 const FenContext = createContext<Fen | undefined>(undefined);
 
 export const FenProvider = ({ children }: { children: ReactNode }) => {
-  const getRandomPosition = (): string => {
-    return fenStrings[Math.floor(Math.random() * fenStrings.length)];
+  const whitePositions = useMemo(() => {
+    return fenStrings.filter((fen) => fen.split(" ")[1] === "w");
+  }, []);
+
+  const blackPositions = useMemo(() => {
+    return fenStrings.filter((fen) => fen.split(" ")[1] === "b");
+  }, []);
+
+  const getRandomPosition = (color: ChessColor): string => {
+    if (color === "random")
+      return fenStrings[Math.floor(Math.random() * fenStrings.length)];
+    else if (color === "white")
+      return whitePositions[Math.floor(Math.random() * whitePositions.length)];
+    else if (color === "black")
+      return blackPositions[Math.floor(Math.random() * blackPositions.length)];
+    else return fenStrings[Math.floor(Math.random() * fenStrings.length)]; // Should never execute
   };
 
   return (
-    <FenContext.Provider value={{ getRandomPosition }}>
+    <FenContext.Provider
+      value={{
+        getRandomPosition,
+      }}
+    >
       {children}
     </FenContext.Provider>
   );
